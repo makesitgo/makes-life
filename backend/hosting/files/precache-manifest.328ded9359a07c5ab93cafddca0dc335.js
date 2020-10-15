906f7f71af5dc6dfe390959ce5160a22ca1f0b9d
Nick Makes <nick@makes.life>
2020-10-15 16:27:29 -0400

    Add frontend

---
+++ backend/hosting/files/precache-manifest.328ded9359a07c5ab93cafddca0dc335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaed2b75b12b53e97690b1ad87cbd26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a042bbf9f58340f01846",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a042bbf9f58340f01846",
+    "url": "/static/js/main.b1d20551.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This site is hosted by <code>MongoDB Realm</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://docs.mongodb.com/realm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
